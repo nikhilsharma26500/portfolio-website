bbbe661d578b455499e315f2265419dd9ac94a5b
Nikhil Sharma <nikhilsharma26500@outlook.com>
2023-01-24 04:28:53 -0500

    sidebar design

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,11 +1,23 @@
 import "./index.scss";
 import Sidebar from "../../components/Sidebar";
+import { Outlet } from "react-router-dom";
 
 const Layout = () => {
   return (
-    <>
+    <div className="App">
       <Sidebar />
-    </>
+      <div className="page">
+
+          {/* Setting up the designer HTML tags on the landing page */}
+          <span className="tags top-tags">&lt;body&gt;</span>
+          <Outlet/>
+          <span className="tags bottom-tags">
+          &lt;/body&gt;
+          <br />
+          <span className="bottom-tag-html">&lt;/html&gt;</span>
+          </span>
+      </div>
+    </div>
   );
 };
 
