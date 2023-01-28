fded84a9a426336d9f346ff9c9eb300cd8c04c8b
Nikhil Sharma <nikhilsharma26500@outlook.com>
2023-01-28 12:56:24 -0500

    Update index.js

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -26,7 +26,7 @@ const Home = () => {
           <p>Currently enrolled in B.Sc. Computer Science at Lassonde School of Engineering, York University. Learning technologies and tools such as <span className="coding">Python, Java, JavaScript, HTML </span>and <span className="coding">CSS</span>. Aspiring to delve deeper into Full Stack technologies and the world of Data Science.</p>
           </h1>
           <a
-            href="https://1drv.ms/b/s!AlaMY3YFkkIqh32HvrQnyZR9_HWH?e=uebCHK"
+            href="https://1drv.ms/b/s!AlaMY3YFkkIqiB7XgZXfv_JIg-t1?e=vACP7H"
             target="_blank"
           >
             <button className="flat-button">RESUME</button>
