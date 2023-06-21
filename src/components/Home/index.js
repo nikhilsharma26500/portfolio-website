eeab897dde8224581aa911aba8dadc2bf985588a
Nikhil Sharma <nikhilsharma26500@outlook.com>
2023-06-21 19:19:10 +0530

    Updated resume

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -26,7 +26,7 @@ const Home = () => {
           <p>Currently enrolled in B.Sc. Computer Science at Lassonde School of Engineering, York University. Learning technologies and tools such as <span className="coding">Python, Java, JavaScript, HTML </span>and <span className="coding">CSS</span>. Aspiring to delve deeper into Full Stack technologies and the world of Data Science.</p>
           </h1>
           <a
-            href="https://1drv.ms/b/s!AlaMY3YFkkIqguksfX2FH92V9h7dNw?e=MVjOQp"
+            href="https://1drv.ms/b/s!AlaMY3YFkkIqguld2AGKzIdKvIw08g?e=Xo4HEW"
             target="_blank"
           >
             <button className="flat-button">RESUME</button>
