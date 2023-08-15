b602f02e41fd1f496762ea446af21459b81fc3f0
Nikhil <nikhilsharma26500@outlook.com>
2023-08-16 03:08:33 +0530

    added stuff on main page

--- src/app/(component)/main.tsx
+++ src/app/(component)/main.tsx
@@ -1,11 +1,21 @@
-import { FC } from 'react'
+import Image from "next/image"
+import Person from "../../../public/main_person3.svg"
+import React from "react"
 
-interface homeProps {
-  
-}
-
-const home: FC<homeProps> = ({}) => {
-  return <div>home</div>
+const home = () => {
+  return (
+    <>
+      <div className="flex items-center justify-between mx-10">
+        <div className="text-slate-300">
+          <p className="text-xl cursive-font">Hello!</p>
+          <h1 className="text-4xl">I am Nikhil Sharma</h1>
+        </div>
+        <div className="">
+          <Image src={Person} alt="main person"/>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default home
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,11 @@ import Link from "next/link"
 export default function Home() {
   return (
     <>
-      <div className="bg-[#242424] flex justify-center items-center h-screen dotted-background">
-        <div className=" border-4 mx-auto border-white h-[80%] w-[80%]">
-        <Main/>
+      <div className="bg-[#242424] flex justify-center items-center h-screen  dotted-background">
+        <div className="mx-auto rounded-lg backdrop-blur-xl bg-[#3f3f3f]/75 h-[80%] w-[80%] shadow-lg shadow-black">
+          <div className="container p-4 mx-auto">
+            <Main />
+          </div>
         </div>
       </div>
     </>
