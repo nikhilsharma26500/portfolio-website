318c989736bf6f7ae7426850c6740dba18ed75fa
Nikhil <nikhilsharma26500@outlook.com>
2023-08-16 09:46:51 +0530

    Added carousel

--- src/app/(component)/main.tsx
+++ src/app/(component)/main.tsx
@@ -5,13 +5,13 @@ import React from "react"
 const home = () => {
   return (
     <>
-      <div className="flex items-center justify-between mx-10">
-        <div className="text-slate-300">
-          <p className="text-xl cursive-font">Hello!</p>
-          <h1 className="text-4xl">I am Nikhil Sharma</h1>
+      <div className="flex items-center justify-between h-max mx-10">
+        <div className="text-slate-200">
+          <h1 className="text-5xl roboto">Nikhil Sharma</h1>
+          <p>A passionate software developer and a budding data scientist</p>
         </div>
         <div className="">
-          <Image src={Person} alt="main person"/>
+          {/* <Image src={Person} alt="main person" className="h-[100%] w-[100%]"/> */}
         </div>
       </div>
     </>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import Main from "./(component)/main"
+import DevProjects from "./(component)/devProjects"
 import Link from "next/link"
 
 export default function Home() {
   return (
     <>
       <div className="bg-[#242424] flex justify-center items-center h-screen  dotted-background">
-        <div className="mx-auto rounded-lg backdrop-blur-xl bg-[#3f3f3f]/75 h-[80%] w-[80%] shadow-lg shadow-black">
+        <div className="mx-auto rounded-lg backdrop-blur-xl bg-[#3f3f3f]/75 h-[80%] w-[80%] shadow-lg shadow-black overflow-auto">
           <div className="container p-4 mx-auto">
-            <Main />
+            {/* <Main /> */}
+            <DevProjects/>
           </div>
         </div>
       </div>
