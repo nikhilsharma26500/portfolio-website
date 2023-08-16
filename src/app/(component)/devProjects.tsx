1572bb2a9e09a857bf7bfa6029e87b8d5a0ed926
Nikhil <nikhilsharma26500@outlook.com>
2023-08-17 00:32:06 +0530

    designing projects page

--- src/app/(component)/devProjects.tsx
+++ src/app/(component)/devProjects.tsx
@@ -1,59 +1,71 @@
+"use client"
+
 import Image from "next/image"
+import React from "react"
 import Pic1 from "../../../public/main_person.svg"
 import Pic2 from "../../../public/main_person2.svg"
 import Pic3 from "../../../public/main_person3.svg"
+import YouTube from "react-youtube"
+import { Roboto_Mono } from "next/font/google"
 
-const devProjects = ({ }) => {
-    return (
-        <>
-            <div className="container mx-auto p-4">
+const roboto_mono = Roboto_Mono({ subsets: ['latin'] })
 
-                <div id="default-carousel" className="relative w-full" data-carousel="slide">
-                    <div className="relative overflow-hidden rounded-lg md:h-96">
-                        <div className="hidden duration-700 ease-in-out" data-carousel-item>
-                            <Image src={Pic1} alt="..." />
-                        </div>
-                        <div className="hidden duration-700 ease-in-out" data-carousel-item>
-                            <Image src={Pic2} alt="..." />
-                        </div>
-                        <div className="hidden duration-700 ease-in-out" data-carousel-item>
-                            <Image src={Pic3} alt="..." />
-                        </div>
-                        <div className="hidden duration-700 ease-in-out" data-carousel-item>
-                            <Image src={Pic1} alt="..." />
-                        </div>
-                        <div className="hidden duration-700 ease-in-out" data-carousel-item>
-                            <Image src={Pic2} alt="..." />
-                        </div>
-                    </div>
-                    <div className="absolute z-30 flex space-x-3 -translate-x-1/2 bottom-5 left-1/2">
-                        <button type="button" className="w-3 h-3 rounded-full" aria-current="true" aria-label="Slide 1" data-carousel-slide-to="0"></button>
-                        <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
-                        <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
-                        <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 4" data-carousel-slide-to="3"></button>
-                        <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 5" data-carousel-slide-to="4"></button>
-                    </div>
-                    <button type="button" className="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
-                        <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                            <svg className="w-4 h-4 text-white dark:text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4" />
-                            </svg>
-                            <span className="sr-only">Previous</span>
-                        </span>
-                    </button>
-                    <button type="button" className="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
-                        <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                            <svg className="w-4 h-4 text-white dark:text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
-                            </svg>
-                            <span className="sr-only">Next</span>
-                        </span>
-                    </button>
-                </div>
-
-            </div>
-        </>
-    )
+const projectaDescription = {
+  "TheGuides": [`• Developed personalized AI chatbot with a vision on helping students and professionals with studying and asking questions.`,
+    `• Utilized OpenAI's API to develop the chatbot, enabling it to receive user input and provide responses.`,
+    `• Developed front-end components on NextJS with Typescript, implementing Tailwind CSS for styling.`,
+    `• Implemented Langchain to chain prompts and utilized recently introduced Function Calling feature of GPT.`,
+    `• Used FastAPI to build a lightweight and fast server that runs the model, leveraging SQLAlchemy to develop database. Hosted on cloud using TiDB Serverless.`,
+    `• Incorporated FAISS along with OpenAI Vector Embeddings for text file vectorization.`]
 }
 
-export default devProjects
+
+const devProjects = () => {
+  // Youtube
+  const videoID = { "theGuides": "https://youtu.be/ytT3h9xZPpU" };
+  const opts = {
+    height: '250px',
+    width: "100%",
+  }
+
+  return (
+    <>
+      <div className="container mx-auto p-4 h-full">
+
+        {/* main box */}
+        <div className="mx-14 text-white h-[100%] ">
+
+          {/* Box 1 */}
+          <div className="w-full  mx-auto flex justify-between items-center">
+            <div className={`${roboto_mono.className}`}>
+              <h1 className="text-7xl">
+                TheGuides
+              </h1>
+              <p className="text-xl font-light">Learning powered by AI</p>
+            </div>
+
+            <div className="shadow-xl shadow-black" style={{ borderRadius: "10px", overflow: "hidden" }}>
+              <YouTube opts={opts} videoId={videoID.theGuides} />
+            </div>
+          </div>
+
+          {/* Box 2 */}
+          <div className="">
+            <div className="container">
+              <h2>
+                Description
+              </h2>
+              <ul>
+                {projectaDescription.TheGuides.map((item, index) => (
+                  <li key={index}>{item}</li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default devProjects;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,8 @@ export default function Home() {
   return (
     <>
       <div className="bg-[#242424] flex justify-center items-center h-screen  dotted-background">
-        <div className="mx-auto rounded-lg backdrop-blur-xl bg-[#3f3f3f]/75 h-[80%] w-[80%] shadow-lg shadow-black overflow-auto">
-          <div className="container p-4 mx-auto">
+        <div className="mx-auto rounded-lg backdrop-blur-xl bg-[#3f3f3f]/75 h-[80%] w-[80%] shadow-xl shadow-black overflow-auto">
+          <div className="container h-full  rounded-xl p-4 mx-auto">
             {/* <Main /> */}
             <DevProjects/>
           </div>
